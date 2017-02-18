0bd883d6f2e17807d2f9114f8207af621870efb4
q296494327 <296494327@qq.com>
2017-02-19 00:35:11 +0800

    MVP架构学习

---
+++ app/src/main/java/com/explam/mvpdemo/view/IShowGirlView.java
@@ -0,0 +1,16 @@
+package com.explam.mvpdemo.view;
+
+import com.explam.mvpdemo.base.BaseView;
+import com.explam.mvpdemo.bean.BaiduBeautyBean;
+
+/**
+ * User: xiemiao
+ * Date: 2017-02-18
+ * Time: 19:18
+ * Desc: V层界面展示数据的接口回调(由V层activity实现)
+ */
+public interface IShowGirlView extends BaseView {
+    void getDataSuccess(BaiduBeautyBean data);
+
+    void getDataFail(String msg);
+}
